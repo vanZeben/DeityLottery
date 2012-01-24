deb0efb3d4263e6f7d1f040f7a7e1b00e15ce453
Ryan vanZeben <vanZeben@live.ca>
2012-01-24 14:39:52 -0500

    Update to intertwine with deityapi more

--- src/com/imdeity/lottery/Lottery.java
+++ src/com/imdeity/lottery/Lottery.java
@@ -1,11 +1,15 @@
 package com.imdeity.lottery;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.SQLDataException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Logger;
 
-import org.bukkit.plugin.Plugin;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,152 +17,119 @@ import com.imdeity.deityapi.Deity;
 import com.imdeity.lottery.cmd.LotteryAdminCommand;
 import com.imdeity.lottery.cmd.LotteryCommand;
 import com.imdeity.lottery.objects.LotteryObject;
-import com.imdeity.lottery.util.*;
 
 public class Lottery extends JavaPlugin {
 
-    protected final Logger log = Logger.getLogger("Minecraft");
-    public static Settings settings = null;
-    public static Settings debug = null;
-    private static int taskId = -1;
+	protected final Logger log = Logger.getLogger("Minecraft");
+	public static FileConfiguration config = null;
+	private static int taskId = -1;
 
-    @Override
-    public void onEnable() {
-        loadSettings();
-        checkPlugins();
-        verifyDatabase();
-        getCommand("lottery").setExecutor(new LotteryCommand());
-        getCommand("lotteryadmin").setExecutor(new LotteryAdminCommand());
-        taskId = getServer().getScheduler().scheduleAsyncRepeatingTask(this,
-                new Announce(), 0, (20 * 60 * 60));
-        out(this.getDescription().getVersion() + " Enabled.");
+	@Override
+	public void onEnable() {
+		Lottery.config = this.getConfig();
+		try {
+			Lottery.config.save(new File("plugins/Lottery/config.yml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		verifyDatabase();
+		getCommand("lottery").setExecutor(new LotteryCommand());
+		getCommand("lotteryadmin").setExecutor(new LotteryAdminCommand());
+		taskId = getServer().getScheduler().scheduleAsyncRepeatingTask(this,
+				new Announce(), 0, (20 * 60 * 60));
+		out(this.getDescription().getVersion() + " Enabled.");
 
-    }
+	}
 
-    @Override
-    public void onDisable() {
-        getServer().getScheduler().cancelTask(taskId);
-        out(this.getDescription().getVersion() + " Disabled.");
-    }
+	@Override
+	public void onDisable() {
+		try {
+			Lottery.config.save(new File("plugins/Lottery/config.yml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    private void loadSettings() {
-        settings = new Settings(this);
-        debug = new Settings(this);
-        debug.loadSettings("debug.yml", "/debug.yml");
-        settings.loadSettings("config.yml", "/config.yml");
-    }
+		getServer().getScheduler().cancelTask(taskId);
+		out(this.getDescription().getVersion() + " Disabled.");
+	}
 
-    private void checkPlugins() {
-        List<String> using = new ArrayList<String>();
-        Plugin test;
+	private void verifyDatabase() {
+		Deity.data
+				.getDB()
+				.Write("CREATE TABLE IF NOT EXISTS "
+						+ Deity.data.getDB().tableName("lottery_", "players")
+						+ " ("
+						+ "`id` int(10) unsigned NOT NULL AUTO_INCREMENT,"
+						+ "`username` varchar(16) NOT NULL,"
+						+ "PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 "
+						+ "COMMENT='Current lottery players log';");
 
-        test = getServer().getPluginManager().getPlugin("Permissions");
-        if (test == null)
-            setSetting("USING_PERMISSIONS", false);
-        else {
-            if (Settings.isUsingPermissions())
-                using.add("Permissions");
-        }
+		Deity.data
+				.getDB()
+				.Write("CREATE TABLE IF NOT EXISTS "
+						+ Deity.data.getDB().tableName("lottery_", "winners")
+						+ " ("
+						+ "`id` int(10) unsigned NOT NULL AUTO_INCREMENT,"
+						+ "`username` VARCHAR( 16 ) NOT NULL ,"
+						+ "`winnings` INT( 10 ) NOT NULL DEFAULT  '0' ,"
+						+ "`time` TIMESTAMP NOT NULL,"
+						+ "`has_claimed` INT( 1 ) NOT NULL DEFAULT '0' ,"
+						+ "PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 "
+						+ "COMMENT='Past Lottery winners.';");
+	}
 
-        test = getServer().getPluginManager().getPlugin("iConomy");
-        if (test == null)
-            setSetting("USING_ICONOMY", false);
-        else {
-            if (Settings.isUsingIConomy())
-                using.add("iConomy");
-        }
-        if (using.size() > 0)
-            out("Using: " + StringMgmt.join(using, ", ") + ".");
-    }
+	public void out(String message) {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		log.info("[" + pdfFile.getName() + "] " + message);
+	}
 
-    private void verifyDatabase() {
-        Deity.data.getDB().Write("CREATE TABLE IF NOT EXISTS " + Settings.getMySQLPlayersTable() + " ("
-                + "`id` int(10) unsigned NOT NULL AUTO_INCREMENT,"
-                + "`username` varchar(16) NOT NULL,"
-                + "PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 "
-                + "COMMENT='Current lottery players log';");
+	public class Announce implements Runnable {
+		public void run() {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Date date = new Date();
+			String[] currTime = dateFormat.format(date).split(" ");
+			String[] lastTime = null;
+			try {
+				lastTime = dateFormat.format(
+						Deity.data
+								.getDB()
+								.Read2("SELECT `time` FROM "
+										+ Deity.data.getDB().tableName(
+												"lottery_", "winners")
+										+ " ORDER BY `id` DESC LIMIT 1")
+								.getDate(0, "time")).split(" ");
+			} catch (SQLDataException e) {
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				Calendar cal = Calendar.getInstance();
+				   cal.add(Calendar.DATE, -1);
+				lastTime = dateFormat.format(cal.getTime()).split(" ");
+			}
+			int lastDate = Integer.parseInt((lastTime[0].split("/"))[2]);
+			int currDate = Integer.parseInt((currTime[0].split("/"))[2]);
 
-        Deity.data.getDB().Write("CREATE TABLE IF NOT EXISTS " + Settings.getMySQLWinnersTable() + " ("
-                + "`id` int(10) unsigned NOT NULL AUTO_INCREMENT,"
-                + "`username` VARCHAR( 16 ) NOT NULL ,"
-                + "`winnings` INT( 10 ) NOT NULL DEFAULT  '0' ,"
-                + "`time` TIMESTAMP NOT NULL,"
-                + "`has_claimed` INT( 1 ) NOT NULL DEFAULT '0' ,"
-                + "PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 "
-                + "COMMENT='Past Lottery winners.';");
-    }
+			int corrHour = Lottery.config.getInt("lottery.TIME");
+			int currHour = Integer.parseInt((currTime[1].split(":"))[0]);
 
-    public void setSetting(String root, Object value) {
-        Settings.setProperty(root, value);
-    }
+			boolean timeIsValid = (currHour == corrHour);
+			boolean timeIsGreater = (currHour > corrHour);
 
-    public void out(String message) {
-        PluginDescriptionFile pdfFile = this.getDescription();
-        log.info("[" + pdfFile.getName() + "] " + message);
-    }
+			boolean dateIsDrawn = (currDate == (lastDate));
+			boolean dateIsValid = ((currDate - 1) >= lastDate);
 
-    public class Announce implements Runnable {
-        public void run() {
-            String[] time = Deity.data.getDB().Read("SELECT NOW()").get(1).get(0)
-                    .split(" ");
-            double currentTime = Double.parseDouble((time[1].split(":"))[0]);
-            boolean timeIsValid = (currentTime == Settings.getTicketTime());
-            boolean timeIsGreater = (currentTime > Settings.getTicketTime());
+			if (LotteryObject.getPot() == 0) {
+				return;
+			}
+			if (dateIsValid && timeIsValid) {
+				// defaults
+				LotteryObject.drawWinner();
+			} else if (dateIsValid && timeIsGreater && !dateIsDrawn) {
+				// behind more then an hour
+				LotteryObject.drawWinner();
+			} else if (dateIsDrawn) {
+				// already drew
+			}
 
-            String[] lastTime = Deity.data.getDB().Read("SELECT * FROM " + Settings.getMySQLWinnersTable()
-                            + " ORDER BY `id` DESC LIMIT 1").get(1).get(3).split(" ");
-            int lastDrawDate = Integer.parseInt((lastTime[0].split("-"))[2]);
-            int currentDrawDate = Integer.parseInt((time[0].split("-"))[2]);
-
-            boolean dateIsDrawn = (currentDrawDate == (lastDrawDate));
-            boolean dateIsValid = ((currentDrawDate - 1) >= lastDrawDate);
-
-            if (Settings.isInDebug()) {
-                String output = "";
-
-                if (LotteryObject.getPot() == 0) {
-                    output += "No Money in pot";
-                    return;
-                }
-                if (timeIsValid && dateIsValid) {
-                    // defaults
-                    LotteryObject.drawWinner();
-                    output += "Lottery Ran on time: " + time[1] + "\n";
-                    output += "lastDraw: " + lastDrawDate + "|"
-                            + "currentDraw " + currentDrawDate;
-
-                } else if (dateIsValid && timeIsGreater && (!dateIsDrawn))  {
-                    // behind more then an hour
-                    LotteryObject.drawWinner();
-                    output += "Lottery was behind today: " + time[1] + "\n";
-                    output += "lastDraw: " + lastDrawDate + "|"
-                            + "currentDraw " + currentDrawDate;
-                } else if (dateIsDrawn) {
-                    // already drew
-                    output += "Lottery already ran today: " + currentTime;
-                } else {
-                    output = "Checking Lottery: " + time[1];
-                }
-                try {
-                    FileMgmt.stringToFile(output, settings.getRootFolder()
-                            + FileMgmt.fileSeparator() + "debug.yml", true);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                if (LotteryObject.getPot() == 0) {
-                    return;
-                }
-                if (timeIsValid && dateIsValid) {
-                    // defaults
-                    LotteryObject.drawWinner();
-                } else if (dateIsValid && timeIsGreater && (!dateIsDrawn))  {
-                    // behind more then an hour
-                    LotteryObject.drawWinner();
-                } else if (dateIsDrawn) {
-                    // already drew
-                }
-            }
-        }
-    }
+		}
+	}
 }
--- src/com/imdeity/lottery/objects/LotteryObject.java
+++ src/com/imdeity/lottery/objects/LotteryObject.java
@@ -1,5 +1,6 @@
 package com.imdeity.lottery.objects;
 
+import java.sql.SQLDataException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -8,148 +9,197 @@ import org.bukkit.entity.Player;
 import com.imdeity.deityapi.Deity;
 import com.imdeity.deityapi.exception.InvalidChannelException;
 import com.imdeity.deityapi.exception.NegativeMoneyException;
+import com.imdeity.deityapi.records.DatabaseResults;
+import com.imdeity.lottery.Lottery;
 import com.imdeity.lottery.util.ChatTools;
-import com.imdeity.lottery.util.Settings;
 
 public class LotteryObject {
 
-    public static int getPot() {
-        String sql = "SELECT COUNT(*) FROM " + Settings.getMySQLPlayersTable()
-                + " ORDER BY `id` DESC LIMIT 1";
+	public static int getPot() {
+		String sql = "SELECT COUNT(*) FROM "
+				+ Deity.data.getDB().tableName("lottery_", "players")
+				+ " ORDER BY `id` DESC LIMIT 1";
 
-        try {
-            return ((Integer.parseInt(Deity.data.getDB().Read(sql).get(1)
-                    .get(0))) * (Settings.getTicketPrice()))
-                    + (Settings.getExtraPot());
-        } catch (Exception ex) {
-            return 0;
-        }
-    }
+		try {
+			HashMap<Integer, ArrayList<Object>> query = Deity.data.getDB().Read(sql);
+			if (query != null) {
+				return (Integer.parseInt(""+query.get(1).get(0))
+						* (Lottery.config.getInt("lottery.ticket_price")) + (Lottery.config
+						.getInt("lottery.pot")));
+			} else {
+				return 0;
+			}
+		} catch (Exception ex) {
+			return 0;
+		}
+	}
 
-    /**
-     * @return
-     */
-    public static void drawWinner() {
-        String sql = "";
-        int winnings = 0;
-        String winner = null;
+	/**
+	 * @return
+	 */
+	public static void drawWinner() {
+		String sql = "";
+		int winnings = 0;
+		String winner = null;
 
-        try {
-            winnings = getPot();
-            sql = "SELECT `username` FROM " + Settings.getMySQLPlayersTable()
-                    + " ORDER BY RAND() LIMIT 1";
-            if (Deity.data.getDB().Read(sql) != null) {
-                winner = Deity.data.getDB().Read(sql).get(1).get(0);
-            } else {
-                System.out.println("[Lottery] Lottery Players table is empty.");
-                if (Settings.isInDebug())
-                    System.out.println("[Lottery Debug] "
-                            + Deity.data.getDB().Read(sql));
-                return;
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+		try {
+			winnings = getPot();
+			sql = "SELECT `username` FROM "
+					+ Deity.data.getDB().tableName("lottery_", "players")
+					+ " ORDER BY RAND() LIMIT 1";
+			if (Deity.data.getDB().Read(sql) != null) {
+				winner = Deity.data.getDB().Read2(sql).getString(0, "username");
+			} else {
+				System.out.println("[Lottery] Lottery Players table is empty.");
+				if (Lottery.config.getBoolean("lottery.debug_mode"))
+					System.out.println("[Lottery Debug] "
+							+ Deity.data.getDB().Read(sql));
+				return;
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-        sql = "INSERT INTO  " + Settings.getMySQLWinnersTable() + " ("
-                + "`username`, `winnings`, `time`)" + "VALUES (" + "'" + winner
-                + "', '" + winnings + "', NOW());";
-        if (winner != null) {
-            Deity.data.getDB().Write(sql);
+		sql = "INSERT INTO  "
+				+ Deity.data.getDB().tableName("lottery_", "winners") + " ("
+				+ "`username`, `winnings`, `time`)" + "VALUES (" + "'" + winner
+				+ "', '" + winnings + "', NOW());";
+		if (winner != null) {
+			Deity.data.getDB().Write(sql);
 
-            double money = winnings;
-            clear();
-            try {
-            	Deity.chat.sendPlayerMessage(Deity.server.getOnlinePlayer(winner), "Congrats! You have won the lottery! Use /lottery claim to claim your winnings");
-                Deity.chat.sendMailToPlayer("[Lottery]", winner,
-                        "You just won the daily lottery worth "
-                                + winnings + " Dei! Claim your winnings with <teal>/lottery claim");
-                Deity.chat
-                		.broadcastHerochatMessage(Settings.getChannelName(), 
-                				"<gold>[Lottery] Drawing the <yellow>ImDeity <gold>Daily Lottery...");
-                Deity.chat
-                        .broadcastHerochatMessage(Settings.getChannelName(),
-                                "<gold>[Lottery] <green>" + winner + " <gold>just won <green>" + money
-                                        + " <gold>dei in the daily Lottery! <teal>/lottery");
-            } catch (InvalidChannelException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("[Lottery] Player field was null");
-        }
-    }
+			double money = winnings;
+			clear();
+			try {
+				Deity.chat
+						.sendPlayerMessage(
+								Deity.server.getOnlinePlayer(winner),
+								"Congrats! You have won the lottery! Use /lottery claim to claim your winnings");
+				Deity.chat
+						.sendMailToPlayer(
+								"[Lottery]",
+								winner,
+								"You just won the daily lottery worth "
+										+ winnings
+										+ " Dei! Claim your winnings with <teal>/lottery claim");
+				Deity.chat
+						.broadcastHerochatMessage(Lottery.config
+								.getString("lottery.channel_name"),
+								"<gold>[Lottery] Drawing the <yellow>ImDeity <gold>Daily Lottery...");
+				Deity.chat
+						.broadcastHerochatMessage(
+								Lottery.config
+										.getString("lottery.channel_name"),
+								"<gold>[Lottery] <green>"
+										+ winner
+										+ " <gold>just won <green>"
+										+ money
+										+ " <gold>dei in the daily Lottery! <teal>/lottery");
+				System.out.println("[Lottery] "+winner+" won todays lottery");
+			} catch (InvalidChannelException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("[Lottery] Player field was null");
+		}
+	}
 
-    public static void claimPrize(Player player) {
-        String sql = "SELECT `winnings` FROM "
-                + Settings.getMySQLWinnersTable() + " "
-                + " WHERE `username` = '" + player.getName() + "' AND "
-                + "`has_claimed` = '0'";
-        HashMap<Integer, ArrayList<String>> query = Deity.data.getDB().Read(sql);
-        if (query == null) {
-            Deity.chat.sendPlayerError(player, "Lottery", "You have no winnings that can be claimed at this time.");
-        } else {
-            double winnings = (double) Integer.parseInt(query.get(1).get(0));
-            try {
-                if (Deity.econ.receive(player.getName(), winnings)) {
-                    Deity.chat.sendPlayerMessage(player, "[*] ", "<green>Thanks for playing in the lottery, "+winnings+" Dei has been added to your account!");
-                    
-                    sql = "UPDATE "+Settings.getMySQLWinnersTable()+ " SET `has_claimed` = 1 WHERE `username` = '"+player.getName()+"'";
-                    Deity.data.getDB().Write(sql);
-                }
-            } catch (NegativeMoneyException e) {e.printStackTrace();}
-        }
-    }
+	public static void claimPrize(Player player) {
+		String sql = "SELECT `winnings` FROM "
+				+ Deity.data.getDB().tableName("lottery_", "winners")
+				+ " WHERE `username` = '" + player.getName() + "' AND "
+				+ "`has_claimed` = '0';";
+		DatabaseResults query = Deity.data.getDB().Read2(sql);
+		if (query == null) {
+			Deity.chat.sendPlayerError(player, "Lottery",
+					"You have no winnings that can be claimed at this time.");
+		} else {
+			double winnings = 0;
+			try {
+				winnings = (double) query.getInteger(0, "winnings");
+			} catch (SQLDataException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				if (Deity.econ.receive(player.getName(), winnings)) {
+					Deity.chat.sendPlayerMessage(player, "[*] ",
+							"<green>Thanks for playing in the lottery, "
+									+ winnings
+									+ " Dei has been added to your account!");
 
-    public static ArrayList<String> getWinners() {
-        ArrayList<String> out = new ArrayList<String>();
+					sql = "UPDATE "
+							+ Deity.data.getDB().tableName("lottery_",
+									"winners")
+							+ " SET `has_claimed` = 1 WHERE `username` = '"
+							+ player.getName() + "'";
+					Deity.data.getDB().Write(sql);
+				}
+			} catch (NegativeMoneyException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-        String sql = "";
-        sql = "SELECT * FROM " + Settings.getMySQLWinnersTable()
-                + " ORDER BY `id` DESC LIMIT 10";
+	public static ArrayList<String> getWinners() {
+		ArrayList<String> out = new ArrayList<String>();
 
-        HashMap<Integer, ArrayList<String>> winners = Deity.data.getDB().Read(
-                sql);
-        for (int i = 1; i <= winners.size(); i++) {
-            out.add(ChatTools.formatUserList(winners.get(i).get(1), winners
-                    .get(i).get(2)));
-        }
-        return out;
-    }
+		String sql = "";
+		sql = "SELECT `username`, `winnings` FROM "
+				+ Deity.data.getDB().tableName("lottery_", "winners")
+				+ " ORDER BY `id` DESC LIMIT 10";
 
-    public static boolean isWinner(String name) {
+		DatabaseResults query = Deity.data.getDB().Read2(sql);
+		for (int i = 0; i <= query.rowCount(); i++) {
+			try {
+				out.add(ChatTools.formatUserList(
+						query.getString(i, "username"),
+						"" + query.getInteger(i, "winnings")));
+			} catch (SQLDataException e) {
+				e.printStackTrace();
+			}
+		}
+		return out;
+	}
 
-        String sql = "";
-        sql = "SELECT * FROM " + Settings.getMySQLWinnersTable()
-                + " ORDER BY `id` DESC LIMIT 10";
+	public static boolean isWinner(String name) {
 
-        HashMap<Integer, ArrayList<String>> winners = Deity.data.getDB().Read(
-                sql);
-        if (winners != null) {
-            for (int i = 1; i <= winners.size(); i++) {
-                if (winners.get(i).get(1).equalsIgnoreCase(name)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+		String sql = "";
+		sql = "SELECT `username` FROM "
+				+ Deity.data.getDB().tableName("lottery_", "winners")
+				+ " ORDER BY `id` DESC LIMIT 10";
 
-    public static int getNumTickets(String name) {
-        String sql = "SELECT * FROM " + Settings.getMySQLPlayersTable()
-                + " WHERE `username` = '" + name + "';";
-        try {
-            return Deity.data.getDB().Read(sql).size();
-        } catch (Exception ex) {
-            return 0;
-        }
-    }
+		DatabaseResults query = Deity.data.getDB().Read2(sql);
+		if (query != null) {
+			for (int i = 1; i <= query.rowCount(); i++) {
+				try {
+					if (query.getString(i, "username").equalsIgnoreCase(name)) {
+						return true;
+					}
+				} catch (SQLDataException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return false;
+	}
 
-    public static void clear() {
-        String sql = "";
+	public static int getNumTickets(String name) {
+		String sql = "SELECT `id` FROM "
+				+ Deity.data.getDB().tableName("lottery_", "players")
+				+ " WHERE `username` = '" + name + "';";
+		try {
+			return Deity.data.getDB().Read2(sql).rowCount();
+		} catch (Exception ex) {
+			return 0;
+		}
+	}
 
-        sql = "TRUNCATE " + Settings.getMySQLPlayersTable() + ";";
-        Deity.data.getDB().Write(sql);
-    }
+	public static void clear() {
+		String sql = "";
+
+		sql = "TRUNCATE " + Deity.data.getDB().tableName("lottery_", "players")
+				+ ";";
+		Deity.data.getDB().Write(sql);
+	}
 }
